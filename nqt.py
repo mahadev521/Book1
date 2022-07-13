29579c38b8ff43a35edeaf6aa0ff01ae6e2fdeda
Mahadev <srimanthmahadev22@gmail.com>
2022-07-13 06:41:34 +0530

    m

--- nqt.py
+++ nqt.py
@@ -1,17 +1,4 @@
-def fun(n,l):
-    for i in range(n-1):
-        if l[i]==l[i+1]:
-            if i!=0:
-                l[i]=(l[i-1]+l[i])/2
-            else:
-                return 'No'
-            continue
-        while l[i]>l[i+1]:
-            l[i]=(l[i]+l[i+1])/2
-            if l[i]==l[i+1]: return 'No'
-    return 'Yes'
-                           
-    
+   
 n=int(input())
 l=[int(x) for x in input().split()]
 print(fun(n,l))
