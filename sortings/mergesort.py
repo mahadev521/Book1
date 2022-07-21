5ae99b902b40be4159c67730783bd6e277d74a6b
Mahadev <srimanthmahadev22@gmail.com>
2022-07-21 20:09:41 +0530

    commit

---
+++ sortings/mergesort.py
@@ -0,0 +1,33 @@
+def divide(arr,si,ei):
+    if si>=ei: return
+    mid= si + (ei-si)//2
+    divide(arr,si,mid)
+    divide(arr,mid+1,ei)
+    conquer(arr,si,mid,ei)
+
+def conquer(arr,si,mid,ei):
+    left=arr[si:mid+1]
+    right=arr[mid+1:ei+1]
+    i,j,k=0,0,si
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            arr[k]=left[i]
+            i+=1
+        else:
+            arr[k]=right[j]
+            j+=1
+        k+=1
+    while i<len(left):
+        arr[k]=left[i]
+        i+=1
+        k+=1
+    while j<len(right):
+        arr[k]=right[j]
+        j+=1
+        k+=1
+    return arr
+
+
+arr=[6,3,9,5,2,8]
+divide(arr,0,len(arr)-1)
+print(arr)
